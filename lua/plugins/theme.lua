47c7024ad6f60844ba971c44de6bbabccb905a4a
circleorange <piotrbielski3@gmail.com>
2024-08-05 02:17:20 +0100

    colour scheme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,29 +2,34 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	opts = {
-		integrations = {
-			cmp = true,
-			flash = true,
-			grug_far = true,
-			lsp_trouble = true,
-			mason = true,
-			markdown = true,
-			mini = true,
-			native_lsp = {
-				enabled = true,
-				underlines = {
-					errors = { "undercurl" },
-					hints = { "undercurl" },
-					warnings = { "undercurl" },
-					information = { "undercurl" },
+	opts = {},
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato",
+			integrations = {
+				cmp = true,
+				flash = true,
+				grug_far = true,
+				lsp_trouble = true,
+				mason = true,
+				markdown = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
 				},
+				navic = { enabled = true, custom_bg = "lualine" },
+				telescope = true,
+				treesitter = true,
+				treesitter_context = true,
+				which_key = true,
 			},
-			navic = { enabled = true, custom_bg = "lualine" },
-			telescope = true,
-			treesitter = true,
-			treesitter_context = true,
-			which_key = true,
-		},
-	},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
